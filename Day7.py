aa5b882e12c2c9985074b44c4684dd145beb9f1a
Binod Sharma <thebinod404@gmail.com>
2025-08-12 10:08:03 +0545

    August 12

---
+++ Day7.py
@@ -0,0 +1,11 @@
+
+a=10
+b=9
+
+print("The Addition Among a and b is" , a+b)
+print("The Substraction Among a and b is" , a-b)
+print("The Multipication Among a and b is" , a*b)
+print("The Division Among a and b is" , a/b)
+print("The Exponential Among a and b is" , a**b)
+print("The Floor Division Among a and b is" , a//b)
+print("The modulus among a and b is" , a%b)
