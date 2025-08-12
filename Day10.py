3061cc0fcf7643174555da40331fe2652ae77fb6
Binod Sharma <thebinod404@gmail.com>
2025-08-12 10:36:03 +0545

    august 12

---
+++ Day10.py
@@ -0,0 +1,10 @@
+#Taking Inputs
+
+# Bydefault It talkes String
+a= input("Enter Your Name: ")
+print("You Have Entered " , a)
+
+# Inputing Integer
+b= int(input("Enter any Number:"))
+print("You Have Entered", b)
+
---
+++ Day9.py
@@ -0,0 +1,8 @@
+# Typecasting 
+
+a = 1
+print("Currently, The Datatype of a is", type(a))
+
+a = str(a) #----> Become String
+print("Currently, The Datatype of a is", type(a))
+
