162af88802a09b883249091bb1f46b39df7cb7a8
Binod Sharma <thebinod404@gmail.com>
2025-08-12 09:59:09 +0545

    August 12

---
+++ Day6.py
@@ -0,0 +1,21 @@
+ # Variables ---->
+
+a=1 #integer
+b=1.1 #float
+c=complex(8,2) #Complex Number
+d="BinodX" #String
+e = True #Boolean
+f=[1, 1.1 , "BinodX"] #List
+g=None #NoneType
+h= {"name":"binodx","isProgrammer":True} #Mapped Data / dict
+i = ("binodx",20,1.1) #Tuple
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
