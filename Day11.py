0323174847ecee94551f2541fd5821318ddc041b
Binod Sharma <thebinod404@gmail.com>
2025-08-13 10:52:44 +0545

    August 13

---
+++ Day11.py
@@ -0,0 +1,24 @@
+# Strings 
+
+name = "binodx"
+print('We Can Use "" inside single Quotes')
+
+
+print(name[0]) #--> Gives First letter of String
+
+# We can use multipleline String through triplequotes
+longText = ''' Lorem Ipsum is simply dummy
+ text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 
+the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+ survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged '''
+
+# To get all characters in long string we use for loop
+
+for characters in longText:
+    print(characters)
+
+
+
+
+
+
