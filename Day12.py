1822963bcaf93f7ee45ac8f0fbcee3e0794826eb
Binod Sharma <thebinod404@gmail.com>
2025-08-13 11:07:28 +0545

    August 13

---
+++ Day12.py
@@ -0,0 +1,11 @@
+# Slicing On String
+
+name = "binodx"
+
+print(len(name)) # Gives Length of string
+
+print(name[1:3]) # It gives the characters from index 1 to index 2 --> in
+
+print(name[-4:-1]) # -4 means Length of String - 4 so it looks like print(name[2:5])
+
+print(name[:]) # It means Starting from 0 and go to length of string
