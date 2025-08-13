786cc47b0ad6f2f91af9a791aaeb327a6eab9991
Binod Sharma <thebinod404@gmail.com>
2025-08-13 11:40:31 +0545

    August 13

---
+++ Day14.py
@@ -0,0 +1,12 @@
+# IConditional Statements
+
+age = int(input("Enter Your Age : "))
+
+if(age <=10):
+    print("You are Kid Now")
+elif(age >=10 and age<=20):
+    print("You are Teenager")
+elif(age>=20 and age<=60):
+    print("You are Adult")
+else:
+    print("You are Old")
