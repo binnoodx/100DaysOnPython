3694d00ea040994acadf5e4eb83a5a1a63e86b23
Binod Sharma <thebinod404@gmail.com>
2025-08-13 11:31:29 +0545

    August 13

---
+++ Day13.py
@@ -0,0 +1,18 @@
+
+ # String Operations 
+
+name = "bin Oodx21!"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.endswith("!"))
+print(name.startswith("a"))
+print(name.rstrip("!")) #---> Remove ! from ending
+print(name.split("o"))
+print(name.count("o"))
+print(name.find("2"))
+print(name.isspace())
+print(name.isalnum())
+print(name.isalpha())
