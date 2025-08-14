2bfcb14448bf4e69c6eba0003f0a7cc87cb1d5c3
Binod Sharma <thebinod404@gmail.com>
2025-08-14 22:12:16 +0545

    August 14

---
+++ Day17.py
@@ -0,0 +1,17 @@
+# For Loop in Python
+
+name = "binodx"
+
+for characters in name:
+    print(characters , end="-")
+
+my_list = ["Java", "Python", "C++" , "C#"]
+
+for lang in my_list:
+    print(lang)
+
+for number in range(1,12 ):
+    print(number )
+
+for i in range(1,30,3):  # Gives Gap of 3 in every Number
+    print(i , end=",")
