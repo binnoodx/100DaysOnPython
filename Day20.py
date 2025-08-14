ea8bd5e79b8a2f7a27e25c8fdf39f8dd94a55319
Binod Sharma <thebinod404@gmail.com>
2025-08-14 22:40:22 +0545

    August 14

---
+++ Day20.py
@@ -0,0 +1,13 @@
+# Functions in Python
+
+num1 = int(input("Enter First Number "))
+num2 = int(input("Enter Second Number "))
+
+def getSum(a,b):
+    print("The Sum is ", a+b)
+
+def uncompleteFunction(a,b):
+    pass #Skip the function and Move Ahead
+
+getSum(num1 , num2) #Function CallBack
+
