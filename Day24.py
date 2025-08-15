caf95c4a581f8a848e8ee020e7b69ad8e4dc8748
Binod Sharma <thebinod404@gmail.com>
2025-08-15 22:44:42 +0545

    august 15

---
+++ Day24.py
@@ -0,0 +1,19 @@
+# Tuple in Python
+
+a= (1,2,3)## Tuple
+print("a is " , type(a))
+
+b = (1)## Int
+print("b is ",type(b))
+
+c = (1,)## Tuple
+print("c is " , type(c))
+
+# a[0] = 10; Tuple are Immutable.So it gives errors
+# print(a)
+
+print(a[2])
+
+d = a[0:2]
+print(d)
+
