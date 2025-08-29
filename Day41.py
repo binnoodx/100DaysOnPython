0697bacc97ea65378e0e67703a788fe9d0155899
Binod Sharma <thebinod404@gmail.com>
2025-08-29 20:15:07 +0545

    August 29

---
+++ Day41.py
@@ -0,0 +1,16 @@
+### ShortHand If Else
+
+a = 10;
+b = 20;
+
+
+# Using Short If Else Method
+print("A") if a>b else print("=") if a==b else print("B")
+
+# Using Traditional If Else Method
+if(a>b):
+    print("A")
+elif(a==b):
+    print("=")
+else:
+    print("B")
