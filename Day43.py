541b8600cc6ab51fbccf7de1e2c85dae24931d66
Binod Sharma <thebinod404@gmail.com>
2025-08-29 20:53:50 +0545

    August 29

---
+++ Day43.py
@@ -0,0 +1,12 @@
+# Virtual Environment in Python
+
+# run these commands
+# python -m venv myenv ---> for making Virtual environments
+# myenv/scripts/activate.bat in Terminal or myenv/scripts/activate.ps1 in Cmd
+# then you can install custom packages version which work for that file only
+# Use deactivate to close virtual environments
+# 
+# pip freeze > requirements.txt ----> Give all the required packages in text File
+# pip install -r requirements.txt ---> Install those packages which are in txt file
+#
+
