a772e69ed2fcd2be7e875478f359b71241f70b8c
Binod Sharma <thebinod404@gmail.com>
2025-08-29 20:17:54 +0545

    August 29

---
+++ Day42.py
@@ -0,0 +1,6 @@
+#enumerate Function in Python
+
+my_list = [22,55,66,69,20]
+
+for index,element in enumerate(my_list , start=0): #Enumerate helps to count Index of members in List
+    print(f"The Index Of {element} is {index}")
