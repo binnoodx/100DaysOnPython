b203b4c12529b0b537b71af1e8f4099bfc2f3282
Binod Sharma <thebinod404@gmail.com>
2025-09-20 08:40:39 +0545

    Sept 20

--- Day68.py
+++ Day68.py
@@ -13,32 +13,20 @@ name.png --> 5.png
 
 import os
 
-loc = r"C:\delete"
+loc = r"C:\\delete" #Put your filder Location here
+
+
 files = os.listdir(loc)
-
-# Function to rename files of a specific extension
-def rename_by_extension(ext):
-    count = 1
-    for file in files:
-        if file.lower().endswith(ext):
-            name, _ = os.path.splitext(file)
-            src = os.path.join(loc, file)
-            dst = os.path.join(loc, f"{count}{ext}")
-            
-            # Handle conflicts by using a temp name first
-            if os.path.exists(dst):
-                dst = os.path.join(loc, f"temp_{count}{ext}")
-
-            os.rename(src, dst)
-            count += 1
-
-# Call for the extensions you care about
-rename_by_extension(".png")
-rename_by_extension(".jpg")
-rename_by_extension(".jpeg")
-rename_by_extension(".txt")
+i=0
+for file in files:
+    if(file.split(".")[1]=="png"):
+        os.rename(f"loc\\{file}" , f"loc\\{i}")
+    i=i+1
 
 print("Success")
+
+
+
 
 
 
---
+++ Day75.py
@@ -0,0 +1,1 @@
+#Attempt to solve Exercise - Clear the Clutter
