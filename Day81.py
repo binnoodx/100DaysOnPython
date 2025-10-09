fa4c1f208e989f1ba33e34d6c8a48970b67a3f7c
Binod Sharma <thebinod404@gmail.com>
2025-10-09 17:06:09 +0545

    Oct 10

---
+++ Day81.py
@@ -0,0 +1,33 @@
+# Hirarchical and Multiple Inheritance
+
+'''
+
+#Hirerarchical
+
+        Base Class 
+            |
+    -----------------
+    |               |
+Derived1        Derived2
+    |               |
+----------          D6
+|   |   |
+D3  D4  D5
+
+
+
+#Hybrid Inheritance
+
+       Base Class
+            |
+    -----------------
+    |               |
+Derived1        Derived2
+    |               |
+    |               |
+    -----------------
+            |
+        Derived 3
+
+
+'''
