0d5fabbef9ec6afb4739839a2d4b2f5a2a520d63
Binod Sharma <thebinod404@gmail.com>
2025-10-12 11:34:46 +0545

    Oct 12

---
+++ Day94.py
@@ -0,0 +1,24 @@
+"""
+
+Write a python program which reminds you of drinking water every hour or two. Your 
+program can Beep
+
+"""
+
+#pip install  AudioPlayer 
+from  audioplayer import AudioPlayer 
+import time
+
+
+
+
+isManAlive = True
+
+while(isManAlive):
+    time.sleep(4)
+    print("\nHey , Drink Some Water. Stay Hydrated !\n")
+    AudioPlayer("beep.mp3").play(block=True)
+
+
+
+
